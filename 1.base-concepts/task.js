2c3eccc53c68e5fe3c22af1af798816fbe9986da
Turbo <dimcode@keemail.me>
2025-03-18 19:14:31 +0000

    conditions deleted

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,29 +2,21 @@
 function solveEquation(a, b, c) {
 	let arr = [];
 	const d = b ** 2 - 4 * a * c;
-	if (d < 0) {
-		return arr;
-	}
 	if (d === 0) {
 		arr.push(-b / (2 * a));
-		return arr;
 	}
 	if (d > 0) {
 		arr.push((-b + Math.sqrt(d)) / (2 * a));
 		arr.push((-b - Math.sqrt(d)) / (2 * a));
-		return arr;
 	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	let monthlyPercent = (percent / 100) / 12;
 	let bodyCredit = amount - contribution;
-  let monthlyPayment;
-	if (bodyCredit == 0) {
-		return bodyCredit;
-	} else {
+  	let monthlyPayment;
     monthlyPayment = bodyCredit * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
     let totalPayment = monthlyPayment * countMonths;
-	  return Number(totalPayment.toFixed(2));
-  }
+	return Number(totalPayment.toFixed(2));
 }
