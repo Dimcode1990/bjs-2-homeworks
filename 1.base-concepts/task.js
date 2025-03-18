cd494057525164769eec8c5be5d11b46280edf23
Turbo <dimcode@keemail.me>
2025-03-18 15:19:44 +0000

    Первое задание выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	const d = b ** 2 - 4 * a * c;
+	if (d < 0) {
+		return arr;
+	}
+	if (d === 0) {
+		arr.push(-b / (2 * a));
+		return arr;
+	}
+	if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+		return arr;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let monthlyPercent = (percent / 100) / 12;
+	let bodyCredit = amount - contribution;
+  let monthlyPayment;
+	if (bodyCredit == 0) {
+		return bodyCredit;
+	} else {
+    monthlyPayment = bodyCredit * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
+    let totalPayment = monthlyPayment * countMonths;
+	  return Number(totalPayment.toFixed(2));
+  }
 }
